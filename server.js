21f54e5a0ddf09afde4b980b50faf00e85d19af1
BintangBintangBintang <gfoxyshd@gmail.com>
2026-09-23 16:18:05 +0700

    fixx

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
-const path = require("path");
+
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
